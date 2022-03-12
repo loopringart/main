470a7c1cc570985a78b351b0df2698dfc6725397
loopringart <loopringart@gmail.com>
2022-03-12 12:03:11 -0600

    Initial push from desktop

---
+++ wp-content/plugins/slim-seo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '3.13.2',
+        'version' => '3.13.2.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3dec915171ba8bb60bf2bfe05d768106fca8056e',
+        'name' => 'elightup/slim-seo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elightup/slim-seo' => array(
+            'pretty_version' => '3.13.2',
+            'version' => '3.13.2.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3dec915171ba8bb60bf2bfe05d768106fca8056e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
